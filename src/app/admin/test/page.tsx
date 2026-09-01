ba47373847ecb622b0e98fff2854fec34f10f8cd
rodcampesan-cpu <rod.campesan@gmail.com>
2026-08-31 23:03:24 -0300

    chore: adiciona pagina de teste

---
+++ src/app/admin/test/page.tsx
@@ -0,0 +1,7 @@
+﻿export default function TestPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl text-green-600 font-bold">A PAGINA DE TESTE CARREGOU!</h1>
+    </div>
+  );
+}
